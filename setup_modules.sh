abe1b97fd05439a5ceed832277ef8ebc861e1cae
Werner Freund <wsfreund@gmail.com>
2016-03-01 14:58:59 -0300

    Compatible with both cores: FastNet and ExMachina.

--- setup_modules.sh
+++ setup_modules.sh
@@ -7,13 +7,13 @@ Usage: ${0##*/} [--dev] [--head]
 Initialize current master module and get child modules on their respective commits
 determined by the master module release.
 
-    -h             display this help and exit
-    --dev          If set to true, then retrieve commited packages with 
-                   git push rights. Of course, this assumes that your
-                   git account has the rights to do so, otherwise it will 
-                   fail. 
-   --head          It will update to the submodules head instead of the used
-                   commit versions stablished to be used by the packge.
+    -h                display this help and return
+    -d|--dev          If set to true, then retrieve commited packages with 
+                      your ssh git push rights. Of course, this assumes that
+                      your git account has the rights to do so, otherwise it
+                      will fail. 
+   -H|--head          It will update to the submodules head instead of the used
+                      commit versions stablished to be used by the packge.
 EOF
 }
 
@@ -41,7 +41,7 @@ while :; do
   case $1 in
     -h|-\?|--help)   # Call a "show_help" function to display a synopsis, then exit.
       show_help
-      exit
+      return
       ;;
     --dev)
       if [ ${2#--} != $2 ]; then
@@ -52,6 +52,13 @@ while :; do
         continue
       fi
       ;;
+    -d=?*|--dev=?*)
+      dev=${1#*=} # Delete everything up to "=" and assign the remainder.
+      ;;
+    -d=|--dev=)   # Handle the case of an empty --dev=
+      echo 'ERROR: "--dev" requires a non-empty option argument.\n' >&2
+      return 1
+      ;;
     --head)
       if [ ${2#--} != $2 ]; then
         head=1
@@ -60,7 +67,13 @@ while :; do
         shift 2
         continue
       fi
-      continue
+      ;;
+    -H=?*|--head=?*)
+      head=${1#*=} # Delete everything up to "=" and assign the remainder.
+      ;;
+    -H=|--head=)   # Handle the case of an empty --head=
+      echo 'ERROR: "--head" requires a non-empty option argument.\n' >&2
+      return 1
       ;;
     --)              # End of all options.
       shift
@@ -75,18 +88,24 @@ while :; do
   shift
 done
 
+git pull
+
 git submodule init
 if test "$dev" -eq "1"; then
   moduleFile=$(mainmodule)/.gitmodules
   sed -i.bak "s_\(\S*url = \)https://github.com/\(.*\)_\1git@github.com:\2_" $moduleFile
-  git submodule sync
-  git update-index --assume-unchanged $moduleFile
 fi
+
+git submodule sync
 
 if test "$head" -eq "0"; then
   git submodule update --recursive
-else
-  git pull --recurse-submodules
+else # head
+  if ! git pull --recurse-submodules
+  then
+    git submodule foreach --recursive checkout master
+    git submodule foreach --recursive git pull origin master
+  fi
 fi
 
 true
